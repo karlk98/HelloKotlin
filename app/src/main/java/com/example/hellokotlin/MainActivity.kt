1703f791bfb81d3a1b1cd7de5e4a924b65dc55be
Karl Kinson <karl.kinson@gmail.com>
2019-08-19 11:44:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun go(v: View) {
+        text.setText("Hello Kotlin!" + "\n" + Date())
+    }
+}
